8dd63c9c64e1803499395bf432d94d34be055cca
Pavan Kalyan Madala <59166856+pavankalyan6m@users.noreply.github.com>
2023-11-02 12:04:46 +0530

    Add files via upload

---
+++ microservicesExampleProject/microservicesExampleProject/src/main/java/com/spring/microservicesExampleProject/MicroservicesExampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.microservicesExampleProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesExampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesExampleProjectApplication.class, args);
+	}
+
+}
